67efa4203cb138dd725f404fbdc55ac94d7db340
EYATUPE <eyatuphilipesamu@gmail.com>
2023-06-28 09:09:42 +0000

    Using hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
-  let task2 = true;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
-    let task2 = false;
+    const task2 = false;
   }
 
   return [task, task2];
-i}
+}
