42c4bfc07b7dc47cd01cc72758bd60dbf3b7fe6f
EYATUPE <eyatuphilipesamu@gmail.com>
2023-08-22 20:09:48 +0300

    Executing basic JS with NodeJS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+// Defining the function
+const displayMessage = (message) => {
+  console.log(message); // Printing the message to STDOUT
+};
+// Calling the function with a sample message
+module.exports = displayMessage;
